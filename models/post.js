d9aee4be0878db5322bc757e5c9ab412fdffc9f3
Danny Reyes <reyesdmusic@gmail.com>
2020-05-20 17:45:59 -0400

    table join for posts and user works

--- models/post.js
+++ models/post.js
@@ -1,19 +1,25 @@
 module.exports = function (sequelize, DataTypes) {
     var Post = sequelize.define("Post", {
-        title: {
-            type: DataTypes.STRING,
+        body: {
+            type: DataTypes.TEXT,
             allowNull: false,
             validate: {
                 len: [1]
             }
         },
-        body: {
-            type: DataTypes.TEXT,
+        author_id: {
+            type: DataTypes.INTEGER,
             allowNull: false,
-            len: [1]
+            validate: {
+                len: [1]
+            }
         }
     });
 
+    Post.associate = models => {
+        Post.belongsTo(models.User, {foreignKey: 'author_id'});
+      
+        }
     // Post.associate = function (models) {
 
     //     Post.belongsTo(models.user, {
